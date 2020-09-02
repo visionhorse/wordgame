75f43c3df565300e0e30790e090dcfcbf258e499
visionhorse <68139954+visionhorse@users.noreply.github.com>
2020-09-02 23:59:52 +0100

    Updates

---
+++ precache-manifest.09ffeda1d180f29c42f65519cf752370.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f5a5e7ab46b19e472686a0148714bd",
+    "url": "/wordgame/index.html"
+  },
+  {
+    "revision": "adf22a914b72a4bf57cf",
+    "url": "/wordgame/static/css/main.dfaa36a1.chunk.css"
+  },
+  {
+    "revision": "95dbc265baf5e7e06240",
+    "url": "/wordgame/static/js/2.5b4a5fa8.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/wordgame/static/js/2.5b4a5fa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf22a914b72a4bf57cf",
+    "url": "/wordgame/static/js/main.0d7fbaf1.chunk.js"
+  },
+  {
+    "revision": "023ce108657ff835a8c8",
+    "url": "/wordgame/static/js/runtime-main.1323f88a.js"
+  }
+]);
